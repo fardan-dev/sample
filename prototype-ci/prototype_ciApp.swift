59a2c26406df2e46188f2da69dab632e534a55cc
fardan-dev <fardan.wardhana@gmail.com>
2022-12-01 14:52:42 +0700

    first init
    update code

---
+++ prototype-ci/prototype_ciApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototype_ciApp.swift
+//  prototype-ci
+//
+//  Created by telkom on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct prototype_ciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
